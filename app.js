243d500762bd97d06ce77be70f951b20baa473e4
hashamuddin1 <hashamlaptop@gmail.com>
2023-09-18 20:53:23 +0500

    learn typescript

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
 var objPerson = {
-  fullName: "hasham uddin",
-  age: 23,
+    fullName: 'hasham uddin',
+    age: 23
 };
-console.log(objPerson);
+var objAdmin = {
+    fullName: 'hasham uddin',
+    age: 23,
+    field: 'Business Analyst',
+    nationality: 'pakistani'
+};
+console.log(objAdmin);
+//Array
+var arr1 = ['hasham', 'ali', 'ahmed'];
+console.log(arr1);
+//Output
+// ['hasham','ali','ahmed']
+var arr2 = ['hasham', 23, false];
+console.log(arr2);
+// Output
+// ['hasham',23,false]
+//enum
+var country;
+(function (country) {
+    country["pakistan"] = "Pakistan";
+    country["india"] = "India";
+    country["bangladesh"] = "Bangladesh";
+})(country || (country = {}));
+console.log(country.pakistan);
+//Union
+var value1 = 'hasham';
+console.log(value1);
+// output
+// hasham
+//literals
+var value2 = 23;
+console.log(value2);
+// output
+// 23
--- app.ts
+++ app.ts
@@ -4,7 +4,12 @@ interface IPerson{
 }
 
 interface IPerson{
-    field?: string
+    field?: string,
+}
+
+
+interface IAdmin extends IPerson{
+    nationality: string
 }
 
 const objPerson:IPerson={
@@ -12,4 +17,45 @@ const objPerson:IPerson={
     age:23
 }
 
-console.log(objPerson)
+const objAdmin:IAdmin={
+    fullName:'hasham uddin',
+    age:23,
+    field: 'Business Analyst',
+    nationality:'pakistani'
+}
+
+console.log(objAdmin)
+
+//Array
+let arr1:string[]=['hasham','ali','ahmed']
+console.log(arr1)
+//Output
+// ['hasham','ali','ahmed']
+
+let arr2:any[]=['hasham',23,false]
+console.log(arr2)
+// Output
+// ['hasham',23,false]
+
+//enum
+
+enum country{
+    pakistan="Pakistan",
+    india="India",
+    bangladesh="Bangladesh"
+}
+
+console.log(country.pakistan)
+
+
+//Union
+let value1:number | string='hasham'
+console.log(value1)
+// output
+// hasham
+
+//literals
+let value2:"hasham"| 23=23
+console.log(value2)
+// output
+// 23
